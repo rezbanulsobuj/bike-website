baed87151cf287db14ff62e7cf504c942c996bb8
rezbanulsobuj <rezbanul.sobuj77@gmail.com>
2022-04-04 15:26:47 +0600

    third

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/DashBoard/Dashboard';
+import FoundNot from './components/FoundNot/FoundNot';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+
 import Reviews from './components/Reviews/Reviews';
 
 
@@ -22,6 +24,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<FoundNot></FoundNot>}></Route>
       </Routes>
 
 
---
+++ src/components/FoundNot/FoundNot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import pageNotFound from '../../images/pageNotFound.png'
+
+const FoundNot = () => {
+    return (
+        <div>
+            <img src={pageNotFound} alt="" />
+        </div>
+    );
+};
+
+export default FoundNot;
